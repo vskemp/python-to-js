d8f23a3cc29d3201c22d7e6da4198a13332c8264
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2019-12-02 11:00:04 -0500

    adds interpolation

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,48 @@
 // This is a comment
-// This is another comment
-/* What? It's a CSS comment! */
+// This is another comment.
 
-// Variable! (Storing single pieces of information)
+/*
+What?
+It looks like a CSS comment!
+Yasss.
+*/
+
+// Variables! (Storing single pieces of info.)
 // turkey_legs_consumed = 3
-var turkey_legs_consumed = 3;
+var turkeyLegsConsumed = 3;
+// 1. Prefix your variables the first time you use them.
+// 2. Use camelCase!
+// 3. Terminate each line with `;`
 
-// prefix your variables the firs time you use them!
-// Use camelCase!
-
-//'var' was the original way to declare a vaeriable
-// 'let' is better because:
+// `var` was the original way to declare variables.
+// `let` is better because:
 // It gives you more control. (You can limit its use to a specific if/else or a specific loop.)
 let turkeysCooked = 2;
 
-//'const' is lkike 'let' but:
-// - You must vassign it when you declare it
+// `const` is like `let`, but:
+// - You must assign it when you declare it.
 // - You can never reassign it.
-const numberofTurkeysICanEat = 1;
+const numberOfTurkeysICanEat = 1;
 
 // How to use these keywords:
-// 1.By default, create your variables using 'const'
-// 2. If yo find out that you need to reassign it, change it to 'let'
+// 1. By default, create your variables using `const`
+// 2. If you find out that you need to reassign it, change it to `let`.
 
-// print("Hello World")
+// print("Hello World!")
 console.log("Hello World!");
 
-// print(turkeysCooked);
+// print(turkeysCooked)
 console.log(turkeysCooked);
+
+// print(f"I ate {turkeyCooked} turkeys!')
+console.log('I ate ${turkeysCooked} turkeys!');
+// You have to use '' to run an f string function in js. 
+// It's good practice to use '' through out even when it doesn't matter
+// You can use backtick-strings for interpolation.
+// - Template Strings
+// - Templpate Literals
+
+// print(f"How many turkeys  I should eat {numberOfTurkeysICanEat - turkeysCooked})
+console.log('Turkeys to eat: ${numberOfTurkeysICanEat - turkeysCooked}');
+
+const catName = 'Sally';
